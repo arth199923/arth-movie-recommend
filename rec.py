c0e8aacb83445e97b3e788255c6a2eb53d702536
Arth Kumar Vaishya <66262483+arth199923@users.noreply.github.com>
2024-05-11 05:50:03 +0530

    rec.py

--- rec.py
+++ rec.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import requests
 
+# Function to fetch movie details from API
 def fetch_movie_details(movie_id):
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=f0876dce92dd28505b9ec945cb32c688')
     data = response.json()
@@ -15,11 +16,13 @@ def fetch_movie_details(movie_id):
         'vote_average': data['vote_average']
     }
 
+# Function to fetch movie poster
 def fetch_poster(movie_id):
     response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=f0876dce92dd28505b9ec945cb32c688')
     data = response.json()    
     return f"https://image.tmdb.org/t/p/original/{data['poster_path']}"
 
+# Function to recommend similar movies
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
@@ -34,17 +37,33 @@ def recommend(movie):
         recommend_movie_details.append(movie_details)
     return recommend_movies, recommend_movie_details
 
+# Load movie data and similarity matrix
 movies_dict = pickle.load(open('movie_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
-
 similarity = pickle.load(open('similarity.pkl','rb'))
 
-st.title('🎬 Discover Your Next Favorite Movie!')
+# Set app title
+st.title('🎬 Discover Your Next Favorite Movie! by Arth')
 
+# Set background image
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url('background.jpg');
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Select a movie from the dropdown
 selected_movie_name = st.selectbox(
     'Find the perfect movie for your mood! Select one from the list below:',
     movies['title'].values)
 
+# Button to trigger recommendations
 if st.button('Recommend'):
     st.markdown('## Recommendations')
     names, details = recommend(selected_movie_name)
